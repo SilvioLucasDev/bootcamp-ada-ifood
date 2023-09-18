19efd924b29851a4de7d460c212ac463245ec149
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-18 20:40:00 -0300

    feat: init project

---
+++ coding-tank/aula-01/intro.js
@@ -0,0 +1,64 @@
+console.log('Conteúdo da página incluída')
+
+// Comentário de uma linha
+
+/* 
+Comentário de 
+várias 
+linhas 
+*/
+
+// Camada de instruções (Comandos)
+
+let nomeCompleto = 'Silvio L. Santos'
+let idade = 24
+
+const pi = 3.14
+console.log(pi * 50)
+
+let alto = true
+console.log(alto)
+alto = false
+console.log(alto)
+
+var a  = 1
+console.log(a)
+
+let animal =  'elefante'
+
+// Dentro das chaves é outro escopo
+{
+    const pi = 2.14
+    console.log(pi)
+    let animal = 'gato'
+    console.log(animal)
+}
+
+console.log(animal)
+
+// var a = 1
+// console.log(a)
+
+// var b = 2, c = 3
+// console.log(b, c)
+
+// var f = g = 5
+// console.log(f , g)
+
+// var i = 6, h = i
+// console.log(h, i)
+
+// var j = k = l = m = 7, n = k 
+// console.log(j, k, l, m, n)
+
+// Exibe um alerta na tela
+// window.alert('Cadastro efetuado com sucesso')
+
+// nome = prompt('Digite seu nome', 'Seu nome aqui')
+
+// console.log(nome)
+
+// console.log('Idade:', idade, 'Nome completo:', nomeCompleto)
+
+// Redireciona o usuário para outra página
+// window.location.href='conteudo.html'
