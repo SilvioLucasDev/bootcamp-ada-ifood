09e292906439072dd0be23f6f934d06f49b82a63
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-15 23:43:01 -0300

    feat: add script aula 03

---
+++ bootcamp/modulo-01/aula-03/script.js
@@ -0,0 +1,15 @@
+const frutas = ["maça", "banana", "abacaxi"];
+const fruta1 = frutas[0];
+
+console.log(frutas[frutas.length - 1]);
+console.log(frutas.at(-1));
+
+const matriz = [
+  [1, 3, 5],
+  [2, 4, 7],
+  [6, 9, 0],
+];
+
+console.log(matriz[2][2]);
+
+console.log(matriz.at(1).at(1));
