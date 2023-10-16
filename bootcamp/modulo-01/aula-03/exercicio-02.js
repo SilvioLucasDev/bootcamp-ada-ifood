43b27a53aac25916da0023502bb49c4dabfd0392
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-16 00:23:45 -0300

    feat: add exercício 2

---
+++ bootcamp/modulo-01/aula-03/exercicio-02.js
@@ -0,0 +1,24 @@
+/*
+    Você é um professor, e tem uma lista com alguns alunos (sendo objetos),
+    que contém nome, nota1 e nota2. Use o for para percorer esse array
+    e calcular a média das duas notas de cada aluno.
+    Ao final, imprimir o nome do aluno e a sua média
+*/
+const listaDeAlunos = [
+  { nome: "João", notas: [8, 7] },
+  { nome: "Maria", notas: [9, 8] },
+  { nome: "Carlos", notas: [6, 5] },
+  { nome: "Ana", notas: [7, 6] },
+  { nome: "Pedro", notas: [10, 9] },
+  { nome: "Lúcia", notas: [8, 7] },
+  { nome: "Fernando", notas: [7, 6] },
+  { nome: "Camila", notas: [9, 8] },
+  { nome: "Rafael", notas: [6, 5] },
+  { nome: "Juliana", notas: [10, 9] },
+];
+
+for (const aluno of listaDeAlunos) {
+  const { nome, notas } = aluno;
+  const media = (notas[0] + notas[1]) / 2;
+  console.log(`Nome: ${nome}: Média = ${media}`);
+}
